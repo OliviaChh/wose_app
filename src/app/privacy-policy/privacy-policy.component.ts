b28a0474d9d0dd349fef7f6138f8a6e0778f2c82
OliviaChh <96041640+OliviaChh@users.noreply.github.com>
2022-09-18 17:35:39 +1000

    adjust code structure in privacy-policy page

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface PrivacyContent{
+  title: string,
+  content: string
+}
+
 @Component({
   selector: 'app-privacy-policy',
   templateUrl: './privacy-policy.component.html',
   styleUrls: ['./privacy-policy.component.scss'],
 })
+
+
 export class PrivacyPolicyComponent implements OnInit {
 
+  privacyContent: PrivacyContent[] = [
+    {
+      title: "1. Clause 1",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra condimentum eget purus in.Consectetur eget id morbi amet amet, in. Ipsum viverra pretium tellus neque. Ullamcorper suspendisse aenean leo pharetra in sit semper et. Amet quam placerat sem."
+    },
+    {
+      title: "2. Clause 2",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra condimentum eget purus in.Consectetur eget id morbi amet amet, in. Ipsum viverra pretium tellus neque. Ullamcorper suspendisse aenean leo pharetra in sit semper et. Amet quam placerat sem."
+    },
+    {
+      title: "3. Clause 3",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra condimentum eget purus in.Consectetur eget id morbi amet amet, in. Ipsum viverra pretium tellus neque. Ullamcorper suspendisse aenean leo pharetra in sit semper et. Amet quam placerat sem."
+    },
+    {
+      title: "4. Clause 4",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra condimentum eget purus in.Consectetur eget id morbi amet amet, in. Ipsum viverra pretium tellus neque. Ullamcorper suspendisse aenean leo pharetra in sit semper et. Amet quam placerat sem."
+    },
+    {
+      title: "5. Clause 5",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra condimentum eget purus in.Consectetur eget id morbi amet amet, in. Ipsum viverra pretium tellus neque. Ullamcorper suspendisse aenean leo pharetra in sit semper et. Amet quam placerat sem."
+    }
+  ];
+  
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+  }
 
   getContent() {
     return document.querySelector('ion-content');
