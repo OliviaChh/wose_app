f174fbd48c7dd287bc68f32cbcae3d5802e8b487
mayama051 <91615485+mayama051@users.noreply.github.com>
2022-09-21 14:37:10 +1000

    Revert "create exercise-live-video"
    This reverts commit 2eab99a37834a9e61a1075a97267b9d8d1aa4487.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,13 +29,9 @@ import { ResetpwComponent } from './resetpw/resetpw.component';
 import { TestListComponent } from './test-list/test-list.component';
 import { TestUpdateComponent } from './test-update/test-update.component';
 import { TestCreateComponent } from './test-create/test-create.component';
-import { ExerciseLiveVideoComponent } from './exercise-live-video/exercise-live-video.component';
+
 
 const routes: Routes = [
-  {
-    path:'exercise-live-video',
-    component: ExerciseLiveVideoComponent
-  },
   {
     path: 'firstpage',
     component: FirstpageComponent
