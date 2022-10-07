1be229b61128a7d0375c46d499ff0a1e26511b9c
OliviaChh <96041640+OliviaChh@users.noreply.github.com>
2022-10-08 02:15:40 +1000

    fix terrible mistake2

--- src/app/service/user_profile.service.ts
+++ src/app/service/user_profile.service.ts
@@ -29,10 +29,6 @@ export class User_profile {
 @Injectable({
     providedIn: 'root'
 })
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 4371483b (Revert "Revert "Merge branch 'main' of https://github.com/OliviaChh/wose_app"")
 
 export class User_profileService {
   
@@ -51,28 +47,15 @@ export class User_profileService {
 
   //backend
   httpOptions = {
-<<<<<<< HEAD
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
   constructor(private httpClient: HttpClient, public formBuilder: FormBuilder) {}
-=======
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-
-  constructor(private httpClient: HttpClient, public formBuilder: FormBuilder,) { 
-
-  }
->>>>>>> parent of 4371483b (Revert "Revert "Merge branch 'main' of https://github.com/OliviaChh/wose_app"")
 
   createUser(user: User_profile): Observable<any> {
     return this.httpClient.post<User_profile>('http://localhost:5000/userprofile/create-user', user, this.httpOptions)
       .pipe(
-<<<<<<< HEAD
         catchError(this.handleError<User_profile>('create UserProfile failed'))
-=======
-        catchError(this.handleError<User_profile>('Error occured'))
->>>>>>> parent of 4371483b (Revert "Revert "Merge branch 'main' of https://github.com/OliviaChh/wose_app"")
       );
   }
 
@@ -82,10 +65,6 @@ export class User_profileService {
       console.log(`${operation} failed: ${error.message}`);
       return of(result as T);
     };
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 4371483b (Revert "Revert "Merge branch 'main' of https://github.com/OliviaChh/wose_app"")
   }  
 
   // final form group
@@ -100,8 +79,4 @@ export class User_profileService {
       goal: ['']
     })
   }
-<<<<<<< HEAD
-=======
-  
->>>>>>> parent of 4371483b (Revert "Revert "Merge branch 'main' of https://github.com/OliviaChh/wose_app"")
 }
