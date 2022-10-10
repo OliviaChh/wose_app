ef51bcb62ee7f7f20b3936b3b3cfbade9affeae1
mayama051 <91615485+mayama051@users.noreply.github.com>
2022-10-10 21:37:58 +1000

    food database

--- server/routes/foods.route.js
+++ server/routes/foods.route.js
@@ -9,7 +9,30 @@ foodsRoute.route('/').get((req, res) => {
             return next(error)
         } else {
             res.json(foods)
-            console.log('Foods retrieved!')
+            console.log('Food retrieved!')
+        }
+    })
+});
+
+foodsRoute.route('/create-food').post((req, res, next) => {
+    FoodsModel.create(req.body, (err, foods) => {
+        if (err) {
+            return next(err)
+        } else {
+            res.json(foods)
+            console.log('Food created!')
+        }
+    })
+});
+
+
+foodsRoute.route('/fetch-food/:id').get((req, res) => {
+    FoodsModel.findById(req.params.id, (err, foods) => {
+        if (err) {
+            return next(err)
+        } else {
+            res.json(foods)
+            console.log('Food fetch!')
         }
     })
 });
